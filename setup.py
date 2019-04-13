bbfd6f89c155aaa07becc01d077a82a69f856828
terapoon <36220075+terapoon@users.noreply.github.com>
2019-04-13 18:56:29 +0900

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ except ImportError:
     from pip.req import parse_requirements
 setup(
     name = 'reversiTool',
-    version='1.0.13',
+    version='1.0.14',
     url = 'https://github.com/reversiWebApp/reversiTools.git',
     license = 'Free',
     author = 'Hiroya Iyori',
